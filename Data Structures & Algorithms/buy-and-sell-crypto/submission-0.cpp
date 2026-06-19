2f86f76697f43844d900b1cee91ac8fdc65390fd
Bhargava Bhatkurse <bhargava.bhatkurse@gmail.com>
2026-06-19 10:13:13 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int two_pointer(vector<int>& prices) {
+        // Cheaper buying price is always better.
+
+        //Two pointer approach
+        int l = 0; //buy
+        int r = 1; //sell
+        int maxprofit = 0;
+
+       for(;r < prices.size();r++) {
+            if(prices[r] > prices[l]) //sell price is better than buy price
+            maxprofit = max(maxprofit, prices[r] - prices[l]);
+            
+            else l = r; //we have found a better buy price
+        }
+         return maxprofit;
+       }
+
+       int maxProfit(vector<int>& prices) {
+       //track lowest price seen so for current i;
+       // DP
+       int n = prices.size();
+       vector<int> lowest(n);
+       vector<int> profit(n);
+
+       lowest[0] = prices[0], profit[0] = 0;
+       for(int i = 1; i < prices.size();i++) {
+            lowest[i] = min(lowest[i-1], prices[i]);
+
+            profit[i] = max(profit[i-1], prices[i] - lowest[i]);
+       }
+       
+         return profit[n-1];
+       }
+};
