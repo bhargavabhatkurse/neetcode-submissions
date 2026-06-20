20c0476310184dbfd798967d25f0b9b2c5fb1406
Bhargava Bhatkurse <bhargava.bhatkurse@gmail.com>
2026-06-20 11:40:45 +0530

    Update: evaluate-reverse-polish-notation - submission-0

--- Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.cpp
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.cpp
@@ -5,7 +5,10 @@ public:
 
         for(auto& t: tokens) {
             if(t != "+" && t != "-" && t != "*" && t != "/") st.push(stoi(t));
-
+              //isdigit() doesnt work here because it expects a character and we have a string
+            //also there can be negative numbers in RPN.
+            //therefore this is the standard way of handling numbers in this.
+            
             else {
                 int j = st.top();
                 st.pop();
