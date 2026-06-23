9bdac6e632a2d7d1b4b8f781b8ef396a8f35a8b8
Bhargava Bhatkurse <bhargava.bhatkurse@gmail.com>
2026-06-23 22:55:38 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        //bruteforce - collect all elements, sort and then rebuild the list
+        vector<int> sorted; //collects the entire sorted array 
+        vector<pair<int,int>> min_array; //it will hold k elements and their list index
+        int n = lists.size();
+        while(true) {
+        vector<bool> valid_index(n,1);
+        int count = n;
+        for(int i = 0; i < lists.size(); i++) {
+            if(lists[i] == nullptr) {
+            valid_index[i] = 0;
+            count--;
+            }
+        }
+        if(count == 0) break;
+        
+        for(int i = 0; i < lists.size(); i++) { //k - list array
+            if(!valid_index[i]) continue; 
+            min_array.push_back({lists[i]->val,i});
+        }
+
+        pair<int,int> min_el = *min_element(min_array.begin(),min_array.end());
+        sorted.push_back(min_el.first);
+        erase(min_array,min_el); //to maintain the list of k elements
+        lists[min_el.second] = lists[min_el.second]->next;
+        }
+
+        ListNode* res = new ListNode(0);
+        ListNode* curr = res;
+        for(int node: sorted) {
+            curr->next = new ListNode(node);
+            curr = curr->next;
+        }
+
+        return res->next;
+
+
+
+
+
+
+
+
+
+
+        
+    }
+};
