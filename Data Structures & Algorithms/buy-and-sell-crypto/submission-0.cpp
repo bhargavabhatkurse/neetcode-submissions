7af36cb6166087df1351ddb7150670d1aa868bdf
Bhargava Bhatkurse <bhargava.bhatkurse@gmail.com>
2026-06-24 12:14:37 +0530

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -21,16 +21,25 @@ public:
        //track lowest price seen so for current i;
        // DP
        int n = prices.size();
-       vector<int> lowest(n);
-       vector<int> profit(n);
+      //  vector<int> lowest(n);
+      //  vector<int> profit(n);
+      //  lowest[0] = prices[0], profit[0] = 0;
+      //  for(int i = 1; i < prices.size();i++) {
+      //       lowest[i] = min(lowest[i-1], prices[i]);
 
-       lowest[0] = prices[0], profit[0] = 0;
+      //       profit[i] = max(profit[i-1], prices[i] - lowest[i]);
+      //  }
+       
+      //    return profit[n-1];
+
+      //same thing but space optimised
+        int lowest = prices[0], profit = 0;
        for(int i = 1; i < prices.size();i++) {
-            lowest[i] = min(lowest[i-1], prices[i]);
-
-            profit[i] = max(profit[i-1], prices[i] - lowest[i]);
+            lowest = min(lowest, prices[i]);
+            profit = max(profit, prices[i] - lowest);
        }
        
-         return profit[n-1];
+         return profit;
+
        }
 };
