f13a7a58aea8deedc147ce8685c975d043d3a7a0
Bhargava Bhatkurse <bhargava.bhatkurse@gmail.com>
2026-06-25 11:24:57 +0530

    Update: lowest-common-ancestor-in-binary-search-tree - submission-0

--- Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.cpp
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.cpp
@@ -12,15 +12,30 @@
 
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode* lowestCommonAncestor_rec(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!(root || p || q)) return nullptr;
 
         if(max(p->val,q->val) < root->val) {
-            return lowestCommonAncestor(root->left,p,q);
+            return lowestCommonAncestor_rec(root->left,p,q);
         }
         else if(min(p->val,q->val) > root->val) {    //it is min here
-            return lowestCommonAncestor(root->right,p,q);
+            return lowestCommonAncestor_rec(root->right,p,q);
         }
         else return root;
+
     }
+
+     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* curr = root;
+        while(curr) {
+        if(max(p->val,q->val) < curr->val) {
+            curr = curr->left;
+        }
+        else if(min(p->val,q->val) > curr->val) {    //it is min here
+            curr = curr->right;
+        }
+        else return curr;
+       }
+        return nullptr;
+        }
 };
