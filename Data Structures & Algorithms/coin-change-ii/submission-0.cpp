db17e94ec0649eb63ae82898e39763484f27b59f
Bhargava Bhatkurse <bhargava.bhatkurse@gmail.com>
2026-07-01 19:24:31 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+vector<vector<int>> dp;
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        dp.assign(n, vector<int>(amount+1,-1));
+        return rec(coins, n-1, amount);
+    }
+    
+    int rec(vector<int> &coins, int i, int amount) {
+    
+        if(i < 0 ) return 0; //no soln: if I have no coin, I can't form the value amount
+        if(amount == 0) return 1; //I have found 1 soln
+        
+        if(dp[i][amount] != -1) return dp[i][amount];
+
+        int res = rec(coins,i-1,amount); //don't take the ith coin
+
+        if(coins[i] <= amount) { //if its possible to take the coin
+            res  += rec(coins, i, amount - coins[i]); //I can reuse
+        }
+
+        return dp[i][amount] = res;
+
+
+    }
+};
