f62aa104b0b78834e0339af8badd0a2c54788ead
Bhargava Bhatkurse <bhargava.bhatkurse@gmail.com>
2026-07-07 11:40:05 +0530

    Update: partition-equal-subset-sum - submission-1

--- Data Structures & Algorithms/partition-equal-subset-sum/submission-1.cpp
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.cpp
@@ -23,6 +23,7 @@ public:
 
         // return curr;
     }
+
     vector<vector<int>> dp; 
     bool rec_topdown(vector<int>& nums,int i, int sum) {
         if(sum == 0) return true;
@@ -50,8 +51,38 @@ public:
         dp.assign(nums.size(),vector<int>(sum/2 + 1,-1));
         return rec_topdown(nums,0,sum/2);
     }
- vector<vector<bool>> Dp; 
+     
+     
+     vector<vector<bool>> Dp; 
+     bool canPartition_forward(vector<int>& nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        int target = sum / 2;
+        int n = nums.size();
+
+        Dp.assign(n + 1, vector<bool>(target + 1, false));
+
+
+        Dp[n][0] = true;
+        //forward dp
+        for(int i = n-1; i >= 0; i--) 
+        for(int j = target; j >= 0; j--) {
+        Dp[i][j] = Dp[i+1][j];
+        if(nums[i] <= j)
+        Dp[i][j] = Dp[i][j] || Dp[i+1][j-nums[i]];
+     }
+
+     return Dp[0][target];
+     }
+
      bool canPartition(vector<int>& nums) {
+        //backward DP
         int sum = 0;
         for (int num : nums) {
             sum += num;
@@ -70,13 +101,13 @@ public:
         }
 
         //forward dp
-        for(int i = n-1; i >= 0; i--) 
-        for(int j = target; j >= 0; j--) {
-        Dp[i][j] = Dp[i+1][j];
-        if(nums[i] <= j)
-        Dp[i][j] = Dp[i][j] || Dp[i+1][j-nums[i]];
+        for(int i = 1; i <= n; i++) 
+        for(int j = 1; j <= target; j++) {
+        Dp[i][j] = Dp[i-1][j];
+        if(nums[i-1] <= j)
+        Dp[i][j] = Dp[i][j] || Dp[i-1][j-nums[i-1]];
      }
 
-     return Dp[0][target];
+     return Dp[n][target];
      }
 };
