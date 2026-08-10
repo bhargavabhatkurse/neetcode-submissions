3fc3579743b4fec3b65599090b66c9a0f8e1907f
Bhargava Bhatkurse <bhargava.bhatkurse@gmail.com>
2026-08-10 18:34:57 +0530

    Add: word-break-ii - submission-0

---
+++ Data Structures & Algorithms/word-break-ii/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    void rec(string s,set<string>& words, vector<string>& path, vector<string>& ans) {
+    if(s.empty()) {
+        string sentence = "";
+
+        for(string word : path)
+            sentence += word + " ";
+
+        sentence.pop_back();
+
+        ans.push_back(sentence);
+
+        return;
+    }
+
+    for(int i = 1; i <= s.size(); i++) { //remember to start from 1
+
+        string word = s.substr(0, i);
+
+        if(words.count(word)) { //valid word
+
+            path.push_back(word);
+
+            rec(s.substr(i),words, path, ans); //find all solutions
+
+            path.pop_back();   // backtrack
+        }
+    }
+}
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        vector<string> ans;
+        vector<string> path;
+        set<string>word(wordDict.begin(),wordDict.end());
+        rec(s,word,path,ans);
+        return ans;
+    }
+};
